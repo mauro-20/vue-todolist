bc8ce8acf0fed4634efa216ca38ae885eb3434ba
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-20 16:19:43 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+  el: '#root',
+  data: {
+    todos: [
+      "fare la spesa",
+      "leggere un libro",
+      "studiare vue"
+    ],
+    newTodo: ''
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodo != '') {
+        this.todos.push(this.newTodo)
+        this.newTodo = ''
+      }
+    }
+  }
+});
